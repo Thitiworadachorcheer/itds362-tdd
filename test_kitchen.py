72513630bde4ed5fbba2781ea4669067e4010347
Chorcheer <Thitiworada.cheer@gmail.com>
2026-09-08 15:08:34 +0700

    error A4

--- test_kitchen.py
+++ test_kitchen.py
@@ -34,6 +34,11 @@ def test_multiplication_returns_a_new_quantity():
     assert result.amount == 600
     assert flour.amount == 200
     assert result is not flour
+    
+def test_equality():
+    assert Quantity(200) == Quantity(200)
+    assert Quantity(200) != Quantity(300)
+
 
 
 
